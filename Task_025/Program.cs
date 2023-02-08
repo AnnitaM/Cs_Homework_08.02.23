1e90a84caca17f1bcae51f11d2f5985bb1f5a484
Annita Maniri <annitk@gmail.com>
2023-02-08 21:19:14 +0200

    A â ñòåïåíè B

---
+++ Task_025/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Enter A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Enter B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.Write($"{A} raised to the power of {B} equals {ARaiseB(A)}");
+
+int ARaiseB(int r)
+{
+    if (B < 0)
+    {
+        B *= (-1);
+    }
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
